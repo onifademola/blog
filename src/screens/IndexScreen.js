45b61900995551faafe927290b5070e1abcce911
Onifade Demola <devep.aonifade@gmail.com>
2019-12-07 17:57:35 +0100

    blog post detail

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,26 +1,47 @@
 import React, { useContext } from 'react';
-import { Text, View, StyleSheet, FlatList, Button } from 'react-native';
+import { Text, View, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 import { Context } from '../context/BlogContext';
 
-const IndexScreen = () => {
-    const {state, addBlogPost} = useContext(Context);
+const IndexScreen = ({navigation}) => {
+    const {state, addBlogPost, deleteBlogPost} = useContext(Context);
 
     return <View>
-        <Text>Blog Index Screen</Text>
         <Button title="Add Post" onPress={addBlogPost} />
         <FlatList 
             data={state}
             keyExtractor={blogPost => blogPost.tiltle}
             renderItem={({item}) => {
-            return <Text>{item.title}</Text>
+                return (
+                    <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                        <View style={styles.row}> 
+                            <Text style={styles.title}>{item.title} - {item.id}</Text>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <Feather style={styles.icon} name='trash'/>
+                            </TouchableOpacity>                        
+                        </View>
+                    </TouchableOpacity>                    
+                );
             }}
         />
     </View>
 };
 
 const styles = StyleSheet.create({
-
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 10,
+        borderBottomWidth: 1,
+        borderColor: 'grey'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
 });
 
 export default IndexScreen;
